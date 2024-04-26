f9124a23ed2cf0f0cb43dd7f26c6d303c170abc1
lexnazgul <50783121+lexnazgul@users.noreply.github.com>
2024-04-26 12:10:17 +0300

    Add files via upload

---
+++ Task_20.py
@@ -0,0 +1,4 @@
+from mypackage.package import say_h
+from mypackage.package import say_b
+say_h()
+say_b()
---
+++ Task_5.py
@@ -0,0 +1,5 @@
+from random import randint
+
+X = randint(1,100)
+
+print(X//1, (X//10)%10, X%10, sep=",")
